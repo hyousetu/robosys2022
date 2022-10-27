0d586364e1301ee030ab51ec4e924cd62398cafd
hyousetu <s21C1078cr@s.chibakoudai.jp>
2022-10-27 12:30:12 +0900

    Add a command

--- test.bash.py
+++ test.bash.py
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyringhtText: 2022 Takato Tanimoto
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -14,6 +14,15 @@ res=0
 out=$(seq 5 | ./plus_stdin.py)
 
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus_stdin.py)
+[ "$?" =1 ]       || ng ${LINENO}
+[ "${out}" ="" ]  || ng ${LINENO}
+
+out=$(echo | ./plus_stdin.py)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" ="" ]  || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
