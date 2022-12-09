a175daf208b6cc5b9978bf14469ed3e86ef58a2f
hyousetu <s21C1078cr@s.chibakoudai.jp>
2022-12-09 14:49:26 +0900

    README.md

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # robosys2022
 ![test](https://github.com/tanimototakato/robosys2022/actions/workflows/test.yml/badge.svg)
 
-このリポジトリにはplusという名前のコマンドがある。
 標準入力からの数字の足し算が出来ます。
 
 ## 開発環境
---
+++ test.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -xv
+# SPDX-FileCopyringhtText: 2022 Takato Tanimoto
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+        res=1
+}
+
+res=0
+
+### I/O TEST ###
+
+out=$(seq 5 | ./plus_stdin.py)
+
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus_stdin.py)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+	               　 
+out=$(echo | ./plus_stdin.py)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+				                              　 
+[ "$res" = 0 ] && echo OK
+exit $res 
